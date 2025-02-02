3b7d00ed4b89c5dfb1012ac1451120f4f80c712e
Subash970 <2005subashk@gmail.com>
2025-02-02 13:45:13 +0530

    nav file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
+//imports
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./static/css/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+//components
+import Nav from "./pages/components/Nav";
 
 //pages
 import Index from "./pages/Index";
@@ -8,11 +12,14 @@ import Index from "./pages/Index";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </BrowserRouter>
+      <main>
+        <Nav />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
     </>
   );
 };
--- frontend/src/pages/Index.js
+++ frontend/src/pages/Index.js
@@ -1,9 +1,5 @@
 const Index = () => {
-  return (
-    <>
-      <p className="h1 text-white bg-dark text-center">Home Screen</p>
-    </>
-  );
+  return <></>;
 };
 
 export default Index;
