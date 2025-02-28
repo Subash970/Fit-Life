4992edbca0d1b8526dbb60142dd37a1c45e9d94f
Subash970 <2005subashk@gmail.com>
2025-02-28 21:19:27 +0530

    slite changes

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 //Signup
 userSchema.statics.signup = async function (email, name, password) {
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,67 @@
+import { useState } from "react";
+import Image from "../static/images/gym.jpg";
+
 const Dashboard = () => {
+  const [soldier, setSoldier] = useState("Soldier");
+
+  const [rep, setRep] = useState(5);
+
   return (
     <>
-      <h1>Dashboard</h1>
+      <div className="container pt-5">
+        <div>
+          <p className="h1 text-center">Welcome {soldier}!</p>
+        </div>
+
+        <div className="workout_detail_container container mt-5">
+          <div className="row py-5">
+            <div className="col-12 col-lg-6">
+              <img
+                src={Image}
+                alt="workout"
+                className="w-100 h-100 rounded"
+                style={{ backgroundSize: "cover", height: "350px" }}
+              />
+            </div>
+            <div className="col-12 col-lg-6 d-flex justify-content-center align-items-center flex-column gap-4 dashboard-text rounded">
+              <p className="h3 fw-bolder">Workout Name</p>
+              <p className="px-5">
+                A good workout balances strength, endurance, and flexibility.
+                Start with a warm-up to prepare your muscles and prevent
+                injuries. Incorporate a mix of cardio, strength training, and
+                core exercises. Focus on proper form and gradually increase
+                intensity for better results. End with a cool-down and
+                stretching to aid recovery and reduce soreness.
+              </p>
+              <p className="fw-bolder">Rep : {rep}</p>
+            </div>
+          </div>
+
+          <div className="row py-5">
+            <div className="col-12 col-lg-6 d-flex justify-content-center align-items-center flex-column gap-4 dashboard-text rounded">
+              <p className="h3 fw-bolder">Workout Name</p>
+              <p className="px-5">
+                A good workout balances strength, endurance, and flexibility.
+                Start with a warm-up to prepare your muscles and prevent
+                injuries. Incorporate a mix of cardio, strength training, and
+                core exercises. Focus on proper form and gradually increase
+                intensity for better results. End with a cool-down and
+                stretching to aid recovery and reduce soreness.
+              </p>
+              <p className="fw-bolder">Rep : {rep}</p>
+            </div>
+
+            <div className="col-12 col-lg-6">
+              <img
+                src={Image}
+                alt="workout"
+                className="w-100 h-100 rounded"
+                style={{ backgroundSize: "cover", height: "350px" }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
